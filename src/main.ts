a715d6f703d10253cc895d652fdb069f161a83c5
Ennio Gonzalez <mr.ennio@gmail.com>
2025-09-07 13:58:43 -0400

    configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ProductsCronJob } from './cronjobs/products.cronjob';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Apply Digital API')
+    .setDescription('API documentation for Apply Digital coding test')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: { persistAuthorization: true },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 
   // Execute the cron job immediately on startup, to make sure
